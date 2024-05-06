fbad67897c865bb1692936224af0f4e52cdfb2de
FivesFifty <134962357+AndrejGockov@users.noreply.github.com>
2024-05-06 18:14:27 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home.js";
+import Paint from "./pages/Paint.js";
+import Settings from "./pages/Settings.js";
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/paint" element={<Paint />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
