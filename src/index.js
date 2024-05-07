6630e660193fa141f0ad705e3c1144f34f7493ff
FivesFifty <134962357+AndrejGockov@users.noreply.github.com>
2024-05-07 03:29:27 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/BrowserRouter";
+// import { BrowserRouter } from "react-router-dom";
 import "./i18n/i18n.js";
 
 //Pages
@@ -11,7 +12,7 @@ import "./style/css/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
