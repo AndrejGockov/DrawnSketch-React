5c123348eef98338c104bb6c61941be7153146b0
FivesFifty <134962357+AndrejGockov@users.noreply.github.com>
2024-05-12 11:07:24 +0200

    adding

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom/BrowserRouter";
 import { BrowserRouter } from "react-router-dom";
 import "./i18n/i18n.js";
 
